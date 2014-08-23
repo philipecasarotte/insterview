c71587cfc8f02b7e09c4204da39184727827e7df
Philipe Casarotte <philipecasarotte@Philipes-MacBook-Pro.local>
2014-08-23 16:13:06 -0700

    cleaning up

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,9 +3,9 @@ class  CommentsController < ApplicationController
   def create
     @photo = Photo.find(params[:photo_id])
     if @comment = @photo.comments.create!(comment_params)
-      flash.now[:notice] = "Thansk for posting!"
+      flash[:notice] = "Thansk for posting!"
     else
-      flash.now[:error] =  "Nope! Nope!"
+      flash[:error] =  "Nope! Nope!"
     end
     redirect_to @photo
   end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,6 @@ class Comment < ActiveRecord::Base
   belongs_to :photo, :counter_cache => true
   
   default_scope { order('created_at DESC') }
+  
+  validates_presence_of :comment, :message => "Come on! You gotta say something."
 end
