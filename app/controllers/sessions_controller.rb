7eea7ed986c73c56cfb7e9e2f323c731c1b6c857
Philipe Casarotte <philipecasarotte@Philipes-MacBook-Pro.local>
2014-08-23 22:51:02 -0700

    facebook connect fixed for rails 4.1.5

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,16 +2,11 @@ class SessionsController < ApplicationController
   def create
     user = User.from_omniauth(env["omniauth.auth"])
     session[:user_id] = user.id
-    redirect_to root_url
+    redirect_to "/"
   end
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_url
-  end
-  
-  private
-  def session_params
-    params.require(:session).permit(:user_id, :provider, :uid)
+    redirect_to "/"
   end
 end
